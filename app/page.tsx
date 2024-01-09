9dcfc3df2aeee5d524c2cf6c817634882ece23df
solamurai <156046077+solamurai@users.noreply.github.com>
2024-01-09 22:08:11 +1000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
-  { name: "ENTER THE DOJO", href: "/dojo" },
+  { name: "ENTER THE DOJO", href: "/contact" },
 ];
 
 export default function Home() {
