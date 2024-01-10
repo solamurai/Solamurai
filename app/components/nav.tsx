c79af9a894a64ed361d068298e14c6065287ab28
solamurai <156046077+solamurai@users.noreply.github.com>
2024-01-10 12:02:03 +1000

    Update nav.tsx

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -29,13 +29,13 @@ export const Navigation: React.FC = () => {
 				<div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto">
 					<div className="flex justify-between gap-8">
 						<Link
-							href="/projects"
+							href="https://twitter.com/solamuraicoin"
 							className="duration-200 text-zinc-400 hover:text-zinc-100"
 						>
 							TWITTER
 						</Link>
 						<Link
-							href="https://twitter.com/solamuraicoin"
+							href=""
 							className="duration-200 text-zinc-400 hover:text-zinc-100"
 						>
 							TELEGRAM
