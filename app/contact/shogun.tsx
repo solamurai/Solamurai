1d48dbb7949264f1c047e9ec94252f70db6489ad
solamurai <156046077+solamurai@users.noreply.github.com>
2024-02-07 14:53:12 +1000

    Update shogun.tsx

--- app/contact/shogun.tsx
+++ app/contact/shogun.tsx
@@ -1,1 +1,20 @@
 
+import Link from "next/link";
+import { Navigation } from "../components/nav";
+
+export default function NewPage() {
+  return (
+    <div
+      className="bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0"
+      style={{
+        backgroundImage: 'url("/shogun.png")',
+        backgroundSize: 'cover',
+        minHeight: '100vh', // Ensure the page takes up the full height of the viewport
+      }}
+    >
+      <Navigation />
+      <h1>This is the New Page</h1>
+      {/* Other content goes here */}
+    </div>
+  );
+}
