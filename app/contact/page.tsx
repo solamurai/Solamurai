ebf0bc200e8208c03192293adc859b5ec31b7ce0
solamurai <156046077+solamurai@users.noreply.github.com>
2024-02-10 10:51:37 +1000

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,13 +3,7 @@ import { Navigation } from "../components/nav";
 
 export default function YourPage() {
   return (
-    <div
-      className="bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0"
-      style={{
-        backgroundImage: 'url("/dojo1.png")',
-        backgroundSize: 'cover',
-      }}
-    >
+    <div className="bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
       <Navigation />
 
       <div className="container flex flex-col items-center justify-center min-h-screen px-4 mx-auto overflow-y-auto mt-20">
@@ -31,10 +25,7 @@ export default function YourPage() {
         </div>
 
         <div className="mt-8 text-white p-8 rounded-md text-center text-shadow relative">
-          {/* Black partially transparent box */}
           <div className="bg-black bg-opacity-75 absolute inset-0 rounded-md"></div>
-          
-          {/* Token Information */}
           <div className="relative z-10">
             <h2 className="text-white text-2xl font-bold mb-4 text-shadow">
               Token Information:
@@ -60,51 +51,7 @@ export default function YourPage() {
             DEX LISTINGS:
           </h2>
           <div className="flex flex-wrap justify-center items-center">
-            <Link href="https://birdeye.so/token/imuB8gpxjqj8EZLSTMMUGtpLgLm51D9VtijjCuewcuj?chain=sola">
-              <a target="_blank">
-                <img
-                  src="/birdeye.png"
-                  alt="Birdeye"
-                  className="w-16 h-16 mx-2 cursor-pointer"
-                />
-              </a>
-            </Link>
-            <Link href="https://dexscreener.com/solana/7memhmneojvbvykmiyhz4mk7yaksha12467q3jgh4sm6">
-              <a target="_blank">
-                <img
-                  src="/dex.png"
-                  alt="DEX"
-                  className="w-16 h-16 mx-2 cursor-pointer"
-                />
-              </a>
-            </Link>
-            <Link href="https://www.geckoterminal.com/solana/pools/7MeMHmNeojVbvykmiYhZ4mK7YaKsHa12467Q3jgH4sM6">
-              <a target="_blank">
-                <img
-                  src="/gecko.png"
-                  alt="Gecko"
-                  className="w-16 h-16 mx-2 cursor-pointer"
-                />
-              </a>
-            </Link>
-            <Link href="https://moontok.io/coins/solamurai-coin">
-              <a target="_blank">
-                <img
-                  src="/moontok.png"
-                  alt="Moontok"
-                  className="w-16 h-16 mx-2 cursor-pointer"
-                />
-              </a>
-            </Link>
-            <Link href="https://ntm.ai/token/imuB8gpxjqj8EZLSTMMUGtpLgLm51D9VtijjCuewcuj?source=post_page-----c89efe8ac7cd--------------------------------">
-              <a target="_blank">
-                <img
-                  src="/NTM.png"
-                  alt="NTM"
-                  className="w-16 h-16 mx-2 cursor-pointer"
-                />
-              </a>
-            </Link>
+            {/* Links to DEX listings */}
           </div>
         </div>
 
@@ -113,24 +60,7 @@ export default function YourPage() {
             BUY/SWAP $SOLA:
           </h2>
           <div className="flex flex-wrap justify-center items-center">
-            <Link href="https://raydium.io/swap/?inputCurrency=sol&outputCurrency=imuB8gpxjqj8EZLSTMMUGtpLgLm51D9VtijjCuewcuj&fixed=in">
-              <a target="_blank">
-                <img
-                  src="/raydium.png"
-                  alt="Raydium"
-                  className="w-16 h-16 mx-2 cursor-pointer"
-                />
-              </a>
-            </Link>
-            <Link href="https://jup.ag/swap/SOLA_imuB8gpxjqj8EZLSTMMUGtpLgLm51D9VtijjCuewcuj-SOL">
-              <a target="_blank">
-                <img
-                  src="/jup.png"
-                  alt="JUP"
-                  className="w-16 h-16 mx-2 cursor-pointer"
-                />
-              </a>
-            </Link>
+            {/* Links to buy/swap $SOLA */}
           </div>
         </div>
 
@@ -139,31 +69,18 @@ export default function YourPage() {
             CRYPTO WALLETS:
           </h2>
           <div className="flex flex-wrap justify-center items-center">
-            <Link href="https://phantom.app/">
-              <a target="_blank">
-                <img
-                  src="/phantom.png"
-                  alt="Phantom"
-                  className="w-16 h-16 mx-2 cursor-pointer"
-                />
-              </a>
-            </Link>
-            <Link href="https://solflare.com/">
-              <a target="_blank">
-                <img
-                  src="/solflare.png"
-                  alt="Solflare"
-                  className="w-16 h-16 mx-2 cursor-pointer"
-                />
-              </a>
-            </Link>
+            {/* Links to crypto wallets */}
           </div>
         </div>
 
         <div className="mt-16" />
 
+        <p className="text-white text-center font-bold italic text-shadow">
+          WARNING: The path of crypto investment is treacherous. Invest wisely, embrace risk, but never more than your soul can endure.
+        </p>
+
         {/* Embedded iframe */}
-        <div className="text-center">
+        <div className="mt-8 text-center">
           <iframe
             src="https://www.retrogames.cc/embed/9729-samurai-shodown-ii-shin-samurai-spirits-haohmaru-jigokuhen-ngm-063-ngh-063.html"
             width="600"
@@ -175,12 +92,6 @@ export default function YourPage() {
             scrolling="no"
           ></iframe>
         </div>
-
-        <p className="text-white text-center font-bold italic text-shadow">
-          WARNING: The path of crypto investment is treacherous. Invest wisely, embrace risk, but never more than your soul can endure.
-        </p>
-
-        <div className="mt-6" />
       </div>
     </div>
   );
